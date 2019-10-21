cbccba51877a2b70e853c4f46950ecec3c98cfbc
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-10-21 13:22:08 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package exercise14;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        String S = in.nextLine();
+        int x = Integer.parseInt(S);
+        System.out.println("Int x: " + x);
+        double y = x;
+        System.out.println("Double y: "+y);
+    }
+}
